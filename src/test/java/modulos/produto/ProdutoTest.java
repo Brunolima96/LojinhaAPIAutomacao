fcdb42e6e9dad552cecf255ecb0c4826a74f085c
Brunolima96 <bruno.1996.lima@hotmail.com>
2022-07-28 22:45:48 -0300

    Inicializando o Projeto de automação de API com Rest-Assured

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,63 @@
+package modulos.produto;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import  static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Testes de API Rest do modulo de Produto")
+public class ProdutoTest {
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do produto")
+    public void testValidarLimitesProibidosdovalordoProduto(){
+
+        //Configurando os dados da API Rest da Lojinha
+        baseURI =  "http://165.227.93.41";
+       // port = 8080
+        basePath = "/lojinha";
+        //Obter o token do usuario admin
+        String token = given()
+                .contentType(ContentType.JSON).body("{\n" +
+                        "  \"usuarioLogin\": \"admin\",\n" +git
+                        "  \"usuarioSenha\": \"admin\"\n" +
+                        "}")
+                .when()
+                    .post("/v2/login")
+                .then()
+                    .extract()
+                        .path("data.token");
+
+
+        // Tentar inserir um produto com valor 0.00 e validar que a mensagem de erro foi apresentada eo
+        // Status code retornado foi 422
+        given().
+                contentType(ContentType.JSON).
+                header("token",token).
+                body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 0.00,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"Preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"string\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    },\n" +
+                        "     {\n" +
+                        "      \"componenteNome\": \"Jogo de futebol\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}").
+                post("/v2/produtos").
+                then().
+                    assertThat()
+                .body("error",equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+
+    }
+}

2b65ada517e1da6011023022e91b7354e50ff685
Brunolima96 <bruno.1996.lima@hotmail.com>
2022-07-29 11:44:48 -0300

    Refatorando o codigo e adicionando novos testes

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -2,6 +2,7 @@ package modulos.produto;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -72,15 +73,17 @@ public class ProdutoTest {
 
     }
 
+    // Tentar inserir um produto com valor 7000.01 e validar que a mensagem de erro foi apresentada eo
+    // Status code retornado foi 422
     @Test
-    @DisplayName("Validar o limite 7000 para o valor do produto")
-    public void testValidarLimitesProibidoSeteMildoProduto() {
+    @DisplayName("Validar o limite 7001 para o valor do produto")
+    public void testValidarLimitesProibidoMaiorqueSeteMildoProduto() {
         given().
                 contentType(ContentType.JSON).
                 header("token", this.token).
                 body("{\n" +
                         "  \"produtoNome\": \"Playstation 5\",\n" +
-                        "  \"produtoValor\": 0.00,\n" +
+                        "  \"produtoValor\": 7000.01,\n" +
                         "  \"produtoCores\": [\n" +
                         "    \"Preto\"\n" +
                         "  ],\n" +
@@ -102,4 +105,72 @@ public class ProdutoTest {
                 body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00")).
                 statusCode(422);
     }
+
+    // Tentar inserir um produto com valor 0.01 e validar que a mensagem  foi apresentada eo
+    // Status code retornado foi 201
+    @Test
+    @DisplayName("Validar o valor 0,01 para o produto")
+    public void testValidarValorOAceitoMaiorQueZero() {
+        given().
+                contentType(ContentType.JSON).
+                header("token", this.token).
+                body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 0.01,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"Preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"string\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    },\n" +
+                        "     {\n" +
+                        "      \"componenteNome\": \"Jogo de futebol\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}").
+                post("/v2/produtos").
+                then().
+                assertThat().
+                body("message", equalTo("Produto adicionado com sucesso"
+                )).statusCode(201);
+
+    }
+
+    // Tentar inserir um produto com valor 7000.00 e validar que a mensagem  foi apresentada eo
+    // Status code retornado foi 201
+    @Test
+    @DisplayName("Validar o valor 7000,00 para o produto")
+    public void testValidaroValorIgualASeteMil() {
+        given().
+                contentType(ContentType.JSON).
+                header("token", this.token).
+                body("{\n" +
+                        "  \"produtoNome\": \"Playstation 5\",\n" +
+                        "  \"produtoValor\": 7000.00,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"Preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"string\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    },\n" +
+                        "     {\n" +
+                        "      \"componenteNome\": \"Jogo de futebol\",\n" +
+                        "      \"componenteQuantidade\": 2\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}").
+                post("/v2/produtos").
+                then().
+                assertThat().
+                body("message", equalTo("Produto adicionado com sucesso")).
+                statusCode(201);
+    }
+
 }
